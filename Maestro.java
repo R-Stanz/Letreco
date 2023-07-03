e568c6f39cabdce04bed4f5f8680a22297a4c465
Renato Stanz <stanz@tutamail.com>
2023-07-03 07:33:16 -0300

    c01 - Adding play method to the maestro and a sketch of the start and getNewHint methods

--- Maestro.java
+++ Maestro.java
@@ -1,10 +1,27 @@
 public class Maestro{
 
-	private Word answer;
-	private Word hint;
-	private Integer tryCount;
-	// private Integer 
+	private Word 		answer;
+	private Word 		hint;
+	private Interface 	messages;
 
 	Maestro(){
 		this.getNewAnswer(); 
 	}
+
+	public void start(){
+		while(true){
+		}
+	}
+
+	private void play(){
+		for(Integer i = 0; i < 6; i++){
+			this.getNewHint();
+			Boolean stopMarker = answer.evaluate(hint);
+			if(stopMarker) break;
+		}
+		messages.printFinalMsg();
+	}
+
+	private void getNewHint(){
+	}
+}
