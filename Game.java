a98534af6394f15984ca38f8c37be10995616642
Renato Stanz <stanz@tutamail.com>
2023-07-03 16:23:02 -0300

    c05 - Started to get implement Game method getNewHint

--- Game.java
+++ Game.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+import java.util.Pattern;
+import java.util.Matcher;
+
 public class Game{
 
 	private Word 		hint;
 	private Interface 	messages;
 	private GameScore	gameScore;
+	Scanner input		= new Scanner(System.in);
 
 	Game(Word answer){
 		for(Integer i = 0; i < 6; i++){
@@ -13,5 +18,22 @@ public class Game{
 	}
 
 	private void getNewHint(){
+		try{
+			Boolean gotAHint = false;
+			while(!gotAHint){
+				String futureHint	= input.nextLine().toLowerCase();
+				Pattern pattern 	= Pattern.compile("^[a-z]{5}$");
+				Matcher matcher 	= pattern.matcher(futureHint);
+				Boolean gotAHint	= matcher.find();
+
+				if(!gotAHint){
+					hint = new Word(futureHint);
+					gotAHint = hint.inDict();
+				}
+			}
+		}
+		catch(Exception e){
+			System.out.println("Invalid hint!");
+		}	
 	}
 }
--- Maestro.java
+++ Maestro.java
@@ -22,7 +22,7 @@ public class Maestro{
 		}
 	}
 
-	private void stop(){
+	private Boolean stop(){
 		messages.printFinalMsg();
 	}
 }
