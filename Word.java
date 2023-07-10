ed01a459532a7a0974405da22da6f4c1c6e60690
Renato Stanz <stanz@tutamail.com>
2023-07-10 11:36:27 -0300

    c08 - Word class with evaluate method implemented

--- Word.java
+++ Word.java
@@ -1,9 +1,8 @@
-import java.util.
-
 public class Word{
 
-	private String 	   word;
-	private Dictionary dict;
+	private Interface	messages; 
+	private String 	   	word;
+	private Dictionary 	dict;
 
 	Word(Dictionary dict){
 		this.dict 	= dict;
@@ -16,6 +15,36 @@ public class Word{
 	}
 
 	public Boolean evaluate(Word anotherWord){
+		ArrayList<Character> wordArr		= this.getCharArrayList();
+		ArrayList<Character> otherWordArr	= anotherWord.getCharArrayList();
+
+		Boolean win = true;
+
+		for(Integer index = 0; index < word.length; index++){
+			wordChar.get(index)		= wordArr.get(index);
+			otherWordChar.get(index)	= otherWordChar.get(index);
+
+			if(wordChar.equals(otherWordChar)){
+				messages.rightPosition();
+				wordArr.remove(wordChar);
+			}
+			else{
+				win = false;
+				if(wordArr.remove(wordChar))
+					messages.rightChar();
+				else
+					messages.wrongChar();
+			}
+		}
+		return win;
+	}
+
+	private ArrayList<Character> getCharArrayList(){
+		ArrayList<Character> wordArrList = new ArrayList<Character>();
+		for(Character c : this.word.toCharArray()){
+			wordArrList.add(c);
+		}
+		return wordArrList;
 	}
 
 	public String toString(){
